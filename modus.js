c9be233fe845f4401923280cf69e3e126bdfe498
Spencer Tipping <spencer@spencertipping.com>
2012-06-11 08:23:43 -0500

    More canonical code

--- modus.js
+++ modus.js
@@ -22,18 +22,12 @@ for(var xi=0,xl=xs.length;
 xi<xl;
  ++xi)x=xs[xi] , (it.append(new_element(x) .val(x) ) ) ;
 return xs} ) .call(this,_) ,it} ) .call(this, (this.empty() ) ) } ) } ,composite:function(paths) {;
-return this.modus(function(_) {return(function(o) {for(var r= {} ,i=0,l=o.length,x;
-i<l;
- ++i)x=o[i] ,r[x[0] ] =x[1] ;
-return r} ) .call(this, ( (function(xs) {var x,x0,xi,xl,xr;
-for(var xr=new xs.constructor() ,xi=0,xl=xs.length;
-xi<xl;
- ++xi)x=xs[xi] ,xr.push( ( [x,find(this,paths[x] ) .first() .val() ] ) ) ;
-return xr} ) .call(this, (function(o) {var ks= [] ;
-for(var k in o)Object.prototype.hasOwnProperty.call(o,k) &&ks.push(k) ;
-return ks} ) .call(this, (paths) ) ) ) ) } ,function(_) {return( (function(xs) {var x,x0,xi,xl,xr;
+return this.modus(function(_) {return(function(xs) {var x,x0,xi,xl,xr;
+var xr=new xs.constructor() ;
+for(var k in xs)if(Object.prototype.hasOwnProperty.call(xs,k) )x=xs[k] ,xr[k] = (find(this,x) .first() .val() ) ;
+return xr} ) .call(this,paths) } ,function(_) {return( (function(xs) {var x,x0,xi,xl,xr;
 for(var x in xs)if(Object.prototype.hasOwnProperty.call(xs,x) )find(this,paths[x] ) .first() .val(_[x] ) ;
 return xs} ) .call(this,paths) ,this) } ) } ,where:find=function(container,path) {;
-return path.constructor===String?container.filter(path) .add(container.find(path) ) 
+return path.constructor===String? (container.filter(path) ) .add(container.find(path) ) 
 :path.constructor===Function?path(container) 
 : (function() {throw new Error( ( 'invalid modus path: ' + (path) + '' ) ) } ) .call(this) } } } ) ;
